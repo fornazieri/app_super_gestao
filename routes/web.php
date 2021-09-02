e7f8c7120081bacd9e1421721d75bacec9a926a1
Vitor Fornazieri <33221274+fornazieri@users.noreply.github.com>
2021-09-02 19:16:52 -0300

    Aprendendo sobre o token CSRF e form via post

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,10 @@ Route::get('/', 'PrincipalController@principal')->name('site.index'); //na versã
 
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 //prefix serve no route para agrupar rodas, ou seja, clientes, fornecedores e produtos estão agrupados
