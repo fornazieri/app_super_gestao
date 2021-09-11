253890907907816f7de9b3c1a84187cc55a7bc08
Vitor Fornazieri <33221274+fornazieri@users.noreply.github.com>
2021-09-11 11:23:06 -0300

    Aprendendo sobre relacionamento 1 para 1 em migrations

---
+++ database/migrations/2021_09_11_111541_create_produto_detalhes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //colunas
+            $table->id();
+            $table->unsignedBigInteger('produto_id'); // por convenção se usa o nome da tabela no singular _ o nome da coluna pk da tabela
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            //constraint
+            $table->foreign('produto_id')->references('id')->on('produtos'); //produto_id faz referencia a coluna id da tabela produtos
+            $table->unique('produto_id'); //para fazer o relacionamento ser de 1 para 1, ou seja, um produto só vai poder ter 1 produto_detalhe
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
